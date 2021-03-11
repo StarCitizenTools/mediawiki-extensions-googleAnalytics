91e51fbacf0676c2cbd3d0e1dec314122fdebd98
alistair3149 <alistair31494322@gmail.com>
2021-03-10 21:49:26 -0500

    Use newer async script

--- includes/GoogleAnalyticsHooks.php
+++ includes/GoogleAnalyticsHooks.php
@@ -29,11 +29,7 @@ class GoogleAnalyticsHooks {
 
 		if ( $wgGoogleAnalyticsAccount !== '' ) {
 			$text .= Html::inlineScript( <<<EOD
-				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
+				window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 				ga('create', '
 				EOD
 				. $wgGoogleAnalyticsAccount . <<<EOD
@@ -43,6 +39,7 @@ class GoogleAnalyticsHooks {
   				ga('send', 'pageview');
 				EOD;
 			, $skin->getOutput()->getCSP()->getNonce() );
+			$text .= "\r\n<script async src='https://www.google-analytics.com/analytics.js'></script>";
 			$appended = true;
 		}
 
